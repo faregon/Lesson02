52edeb72a5dadb58551d6a3c4c637acbd7c2a68a
Faregon <kunobelin@yahoo.com>
2016-02-10 09:47:51 +0100

    first commit

---
+++ Lesson02/example01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Being
+{
+    static int count = 0;
+
+    public Being()
+    {
+        count++;
+        Console.WriteLine("Being is created");
+    }
+    public void GetCount()
+    {
+        Console.WriteLine("There are {0} Beings", count);
+    }
+}
+
+public class Human : Being
+{
+    public Human()
+    {
+        Console.WriteLine("Human is created");
+    }
+}
+public class Animal : Being
+{
+    public Animal()
+    {
+        Console.WriteLine("Animal is created");
+    }
+}
+public class Dog : Animal
+{
+    public Dog()
+    {
+        Console.WriteLine("Dog is created");
+    }
+}
+public class Inheritance2
+{
+    static void Main()
+    {
+        new Human();
+        Dog dog = new Dog();
+        dog.GetCount();
+    }
+}
